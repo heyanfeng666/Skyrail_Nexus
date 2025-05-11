78eecf359b4604cfaca90fe7045c38958e44afa9
heyanfeng666 <153952733+heyanfeng666@users.noreply.github.com>
2025-05-11 14:07:38 +0800

    Initial commit

---
+++ src/client/java/com/skyrail_nexus/SkyRailNexusDataGenerator.java
@@ -0,0 +1,11 @@
+package com.skyrail_nexus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkyRailNexusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
